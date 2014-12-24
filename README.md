5765c1f78ec56dfc66ac531fa80d2fc66b3c851c
David Konsumer <konsumer@jetboystudio.com>
2014-12-24 02:17:19 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### development
+
+- `make setup` - install the build tools
+- `make build` - compress CSS & javascript with browserify & less
+- `make` - light dev build, no minification
+- `make clean` - delete built files
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+var $ = jQuery = require('../../bower_components/jquery/dist/jquery.js');
+
+require('../../bower_components/bootstrap/js/transition.js');
+require('../../bower_components/bootstrap/js/alert.js');
+require('../../bower_components/bootstrap/js/button.js');
+require('../../bower_components/bootstrap/js/carousel.js');
+require('../../bower_components/bootstrap/js/collapse.js');
+require('../../bower_components/bootstrap/js/dropdown.js');
+require('../../bower_components/bootstrap/js/modal.js');
+require('../../bower_components/bootstrap/js/tooltip.js');
+require('../../bower_components/bootstrap/js/popover.js');
+require('../../bower_components/bootstrap/js/scrollspy.js');
+require('../../bower_components/bootstrap/js/tab.js');
+require('../../bower_components/bootstrap/js/affix.js');
+
